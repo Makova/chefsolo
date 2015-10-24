f9d18a31bb70df47302587bfb46fc1fad7c64b36
Manu <makova65@gmail.com>
2015-10-24 22:27:36 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 sudo apt-get update 
-sudo apt-get upgrade -y 
-sudo apt-get install git -y
-cd
+sudo apt-get -y install git
+
 curl https://www.opscode.com/chef/install.sh | sudo bash
